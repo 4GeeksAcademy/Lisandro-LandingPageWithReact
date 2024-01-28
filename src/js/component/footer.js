24780207d3cf08b11d5b1fa7c631ee791f2cb320
lisandromariano <145061399+lisandromariano@users.noreply.github.com>
2024-01-28 10:36:51 +0000

    Proyecto terminado

---
+++ src/js/component/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer(){
+    return (
+        
+            <div className="bg-dark d-flex justify-content-center align-items-center py-3">
+                <span className="text-white">
+                    Copyrigth © Your Website 2019
+                </span>
+             </div>
+    );
+}   
+
+export default Footer
